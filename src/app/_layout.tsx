f6229adba96a5a6383584fd735f33586605a3ed9
Florian Hans <hans@hm.edu>
2026-06-04 14:45:22 +0200

    Removed navbar from phone

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,32 @@
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { AppState } from 'react-native';
+import * as NavigationBar from 'expo-navigation-bar';
 
 export default function RootLayout() {
-  return <Stack>
+    useEffect(() => {
+    const removeNavBar = async () => {
+      NavigationBar.setVisibilityAsync('hidden');
+      NavigationBar.setBehaviorAsync('overlay-swipe');
+    };
+  
+    removeNavBar();
+
+    const subscription = AppState.addEventListener("change", (state) => {
+      if (state === "active") {
+        removeNavBar();
+      }
+    });
+
+    return () => subscription.remove();
+  }, []);
+
+  return <Stack
+        screenOptions={{
+          headerShown: false,
+          statusBarStyle: "light"
+        }}
+      >
         <Stack.Screen name='(tabs)' />
       </Stack>;
 };
