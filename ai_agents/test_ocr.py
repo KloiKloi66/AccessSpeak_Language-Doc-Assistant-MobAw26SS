d02b611249d2cb236dcbd53e844a5080362f1806
Vincent Nier <vincent.nier1@hm.edu>
2026-06-26 03:27:10 +0200

    Replace my path for tesseract with replacable text

--- ai_agents/test_ocr.py
+++ ai_agents/test_ocr.py
@@ -2,7 +2,7 @@ from PIL import Image
 import pytesseract
 
 pytesseract.pytesseract.tesseract_cmd = (
-    r"C:/Users/Vincent/AppData/Local/Programs/Tesseract-OCR/tesseract.exe"
+    r"*YOUR TESSERACT SAVE LOCATION*"
 )
 
 image = Image.open("test.png")
