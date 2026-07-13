b4beb966302ded37be4d557ec1faa37d6e1a275a
Florian Hans <florian7373yaho@gmail.com>
2026-07-13 14:56:38 +0200

    feat(stt): added audioService to start and stop voice recording

---
+++ src/utils/audioService.ts
@@ -0,0 +1,51 @@
+import { Audio } from "expo-av";
+
+let recording: Audio.Recording | null = null;
+
+console.log("AudioService geladen");
+
+export async function startRecording() {
+    console.log("Starte Aufnahme...");
+
+    const permission = await Audio.requestPermissionsAsync();
+
+    if (!permission.granted) {
+        throw new Error("Keine Mikrofonberechtigung");
+    }
+
+    await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+    });
+
+    recording = new Audio.Recording();
+
+    await recording.prepareToRecordAsync(
+        Audio.RecordingOptionsPresets.HIGH_QUALITY
+    );
+
+    await recording.startAsync();
+
+    console.log("Aufnahme gestartet");
+}
+
+export async function stopRecording() {
+
+    if (!recording) {
+        throw new Error("Keine Aufnahme aktiv");
+    }
+
+    await recording.stopAndUnloadAsync();
+
+    const uri = recording.getURI();
+
+    recording = null;
+
+    if (!uri) {
+        throw new Error("Keine Audio-Datei erstellt");
+    }
+
+    console.log("Audio URI:", uri);
+
+    return uri;
+}
